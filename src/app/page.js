8df08d4631fe65be226b830fbb8c0a98a8a6c63d
Zproject312 <projectt924@gmail.com>
2024-12-17 20:45:49 +0500

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Button from "./components/Button";
+import ExploreMenu from "./components/ExploreMenu";
+import Footer from "./components/Footer";
+export default function Home() {
+  return (
+   <div>
+    <Navbar/>
+    <Hero/>
+    <Button/>
+    <ExploreMenu/>
+    <Footer/>
+    </div>
+  );
+}
